baa8a96e0811842d0297bc07e44023ac75e931a6
albina <albinabist1993@mail.ru>
2021-07-20 13:41:26 +0300

    разделила стили на части

---
+++ src/js/burger.js
@@ -0,0 +1,11 @@
+document.getElementById('burger').addEventListener('click', function () {
+    var openMenu = document.querySelector('.header__nav');
+    openMenu.classList.toggle('header__nav--open');
+
+    document.querySelector('#firstLine').classList.toggle('burger__line--first');
+    document.querySelector('#secondLine').classList.toggle('burger__line--second');
+    document.querySelector('#thirdLine').classList.toggle('burger__line--third');
+    
+})
+
+
